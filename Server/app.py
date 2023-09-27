040733a0744ae97d23c3f4da27ced6c7d76f3c2a
codedeamon1 <105770605+codedeamon1@users.noreply.github.com>
2023-09-27 11:59:03 +0530

    done

--- Server/app.py
+++ Server/app.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from api.user import user_route
-from api.instance import instance_route
+from api.instance import instance_route 
 from api.payment import payment_route
-from mongoengine import connect
-from dotenv import load_dotenv
+from mongoengine import connect 
+from dotenv import load_dotenv 
 load_dotenv()
 secret = os.environ.get('TOKEN_SECRET')
 
@@ -17,7 +17,7 @@ app.register_blueprint(payment_route)
 app.secret_key = secret
 CORS(app)
 
-connect(host='mongodb://localhost:27017/chat_bot')
+connect(host=os.environ.get('MONGO_URI'))
 
 if __name__ == "__main__":
     app.debug = True
