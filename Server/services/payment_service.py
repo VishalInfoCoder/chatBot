0c8bddb979fa5a2c621a9469da073214e8775783
codedeamon1 <105770605+codedeamon1@users.noreply.github.com>
2023-10-05 11:17:11 +0530

    New pagination added

--- Server/services/payment_service.py
+++ Server/services/payment_service.py
@@ -145,7 +145,13 @@ def updateChatBot(chatbot_id,plan):
 def view_all_transactions(userdata):
     try:
         myResponse=[]
-        data=User_invoices.objects(user_id=session['user_id'])
+        if userdata['page']:
+            page=userdata['page']
+        else:
+            page=0  
+        per_page=10      
+        skip = (page - 1) * per_page
+        data=User_invoices.objects(user_id=session['user_id']).skip(skip).limit(per_page)
         myResponse.extend([{'_id':str(transaction_data.id),'user_id': str(transaction_data.user_id), 'total_amount': transaction_data.total_amount, 'basic_amount': transaction_data.basic_amount, 'tax_percentage': transaction_data.tax_percentage, 'total_tax_values': transaction_data.total_tax_values, 'cgst': transaction_data.cgst,'sgst': transaction_data.sgst, 'invoice_number': transaction_data.invoice_number,'payment_details': transaction_data.payment_details, 'created': transaction_data.created} for transaction_data in data])
         return make_response({"data":myResponse,"status":True}, 200)
     except Exception as e:
